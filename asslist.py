26bdeacc42e584f95c540c34eaf38140da77798a
Ranjithkumar Viswanathan <138715287+RKReddy212@users.noreply.github.com>
2023-09-09 14:28:16 +0530

    Create asslist.py

---
+++ asslist.py
@@ -0,0 +1,26 @@
+# assingment
+
+# make one list [1,2,3,'abc',3.4,'xyz',5.6]
+
+#add 2 at the 3rd position
+
+#remove 'abc'
+
+#add 10 at end
+
+#fetch the 4th value in the list
+
+
+k = [1,2,3,'abc',3.4,'xyz',5.6]
+
+k.insert(3, 2)
+
+k.remove('abc')
+
+k.append(10)
+
+
+print(k)
+
+print(k[4])
+
